4bc16b3eac972cfe371075b2e99c7318052791c9
CoLoDot <36201752+CoLoDot@users.noreply.github.com>
2019-03-27 21:41:45 +0100

    Delete form.py

--- form.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.forms import forms
-
-
-class Connexion(forms.Form):
-    username = forms.CharField(label="Nom d'utilisateur", max_length=30)
-    email = forms.CharField(label="Mail", max_length=100)
-    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput)
