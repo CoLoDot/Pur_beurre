ff801e8ad14f9c41989639fdede6f07b59e86480
CoLoDot <colodotgo@gmail.com>
2019-04-08 11:54:27 +0200

    Add comments in tests

--- substitut/tests.py
+++ substitut/tests.py
@@ -12,34 +12,42 @@ from .form import Connexion
 class PageTestCase(TestCase):
 
     def test_index_page(self):
+        """Test if Index Page return status code 200"""
         response = self.client.get(reverse('index'))
         self.assertEqual(response.status_code, 200)
 
     def test_mentionlegales_page(self):
+        """Test if Mention Légales Page return status code 200"""
         response = self.client.get(reverse('substitut:mentionslegales'))
         self.assertEqual(response.status_code, 200)
 
     def test_search_page(self):
+        """Test if Search Page return status code 200"""
         response = self.client.get(reverse('substitut:search'))
         self.assertEqual(response.status_code, 200)
 
     def test_signup_page(self):
+        """Test if SignUp Page return status code 200"""
         response = self.client.get(reverse('substitut:signup'))
         self.assertEqual(response.status_code, 200)
 
     def test_login_page(self):
+        """Test if Login Page return status code 200"""
         response = self.client.get(reverse('substitut:login'))
         self.assertEqual(response.status_code, 200)
 
     def test_logout_page(self):
+        """Test if Logout Page return status code 200"""
         response = self.client.get(reverse('substitut:logout'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_account_page(self):
+        """Test if User Account Page return status code 200"""
         response = self.client.get(reverse('substitut:useraccount'))
         self.assertEqual(response.status_code, 200)
 
     def test_user_products_page(self):
+        """Test if User Products Page return status code 200"""
         response = self.client.get(reverse('substitut:userproducts'))
         self.assertEqual(response.status_code, 200)
 
@@ -54,11 +62,13 @@ class DetailPageTestCase(TestCase):
         self.product = Products.objects.get(name="Choco")
 
     def test_detail_page_returns_200(self):
+        """Test if Detail Page return status code 200"""
         product_id = self.product.id
         response = self.client.get(reverse('substitut:detail', args=(product_id,)))
         self.assertEqual(response.status_code, 200)
 
     def test_detail_page_returns_404(self):
+        """Test if Detail Page return status code 404"""
         product_id = self.product.id + 1
         response = self.client.get(reverse('substitut:detail', args=(product_id,)))
         self.assertEqual(response.status_code, 404)
@@ -81,12 +91,14 @@ class SignupPageTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_form_is_valid(self):
+        """Test if Form is valid"""
         form = Connexion(data={'username': 'Coco',
                                'email': self.contact.email,
                                'password': self.contact.password})
         self.assertTrue(form.is_valid())
 
     def test_form_not_valid(self):
+        """Test if Form is not valid"""
         form = Connexion(data={'username': 'Coco',
                                'email': self.contact.email,
                                'password': None})
@@ -102,12 +114,14 @@ class LoginLogoutTestCase(TestCase):
         self.test_user = User.objects.create_user(self.username, self.email, self.password)
 
     def test_user_login(self):
+        """Test if a User is logged in"""
         login = self.client.login(username=self.username, password=self.password)
         response = self.client.post(reverse('substitut:login'))
         self.assertEqual(login, True)
         self.assertEqual(str(response.context['user']), self.username)
 
     def test_user_logout(self):
+        """Test if a User is logged Out"""
         login = self.client.login(username=self.username, password=self.password)
         response = self.client.post(reverse('substitut:login'))
         user_logged = str(response.context['user'])
@@ -130,12 +144,14 @@ class SavingPageTestCase(TestCase):
         self.product = Products.objects.get(name='Chocolat Chaud')
 
     def test_saving_product_is_registered(self):
+        """Test if a product is saved in user's DB"""
         product_id = self.product.id
         save_to_db = Saving.objects.create(contact=self.contact.email,
                                            product_key=self.product.id)
         self.assertEqual(product_id, save_to_db.product_key)
 
     def test_saving_product_not_registered(self):
+        """Test if a product is not saved in user's DB"""
         product_id = self.product.id
         save_to_db = Saving.objects.create(contact=self.contact.email,
                                            product_key=product_id + 1)
