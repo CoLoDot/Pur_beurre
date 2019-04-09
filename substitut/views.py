eb84e71df50b0836051a0d8cf0e063f4c46bec94
CoLoDot <colodotgo@gmail.com>
2019-04-09 16:13:28 +0200

    Update view Search and udpate the search algorithm

--- substitut/views.py
+++ substitut/views.py
@@ -1,12 +1,13 @@
 import json
 import requests
+import random
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
-from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.shortcuts import render, get_object_or_404
 
 from .form import Connexion
 from .models import Products, Users, Saving
+from .pagination import customizePagination
 
 
 # Create your views here.
@@ -65,7 +66,7 @@ def userlogin(request):
 
 
 def userlogout(request):
-    """Logout a registred user"""
+    """Logout a registered user"""
     logout(request)
     return render(request, 'substitut/logout.html', locals())
 
@@ -91,7 +92,6 @@ def products(request):
 
 def userproducts(request):
     """Return saved products of a user"""
-
     try:
         user = request.user.email
         products_to_display = Saving.objects.filter(contact=user)
@@ -100,20 +100,9 @@ def userproducts(request):
         for item in products_to_display:
             keys_list.append(item.product_key)
 
-        product = Products.objects.filter(pk__in=keys_list)
-
-        paginator = Paginator(product, 6)
-        page = request.GET.get('page')
-
-        try:
-            product = paginator.page(page)
-        except PageNotAnInteger:
-            product = paginator.page(1)
-        except EmptyPage:
-            product = paginator.page(paginator.num_pages)
-
+        product_filter = Products.objects.filter(pk__in=keys_list)
+        product = customizePagination(request, product_filter, 6)
         context = {"product": product}
-
     except:
         product = None
         context = {"product": product}
@@ -129,26 +118,19 @@ def search(request):
                          3: 'c',
                          4: 'd',
                          5: 'e'}
+
     if not query:
         product_list = Products.objects.all()
-        paginator = Paginator(product_list, 3)
-        page = request.GET.get('page')
-
-        try:
-            product = paginator.page(page)
-        except PageNotAnInteger:
-            product = paginator.page(1)
-        except EmptyPage:
-            product = paginator.page(paginator.num_pages)
-
+        product = customizePagination(request, product_list, 6)
         context = {"product": product}
     else:
         product_list = Products.objects.filter(name__icontains=query).order_by('nutriscore')
 
         if not product_list:
-            search_url = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?action=process&search_terms="
-                                      + query + "&sort_by=unique_scans_n&page_size=20&json=1")
-            response = json.loads(search_url.text)
+            search_one_product = requests.get("https://fr.openfoodfacts.org/cgi/search.pl?action=process&search_terms="
+                                              + str(query) + "&sort_by=unique_scans_n&page_size=20&json=1")
+            response = json.loads(search_one_product.text)
+
             for product_index in range(0, int(response['count'])):
                 if response['products'][product_index]['states_tags'][1] == 'en:complete':
                     try:
@@ -170,27 +152,35 @@ def search(request):
                                 get_nutriscore = key
                     except KeyError:
                         get_nutriscore = ''
-
-                    if product_index == 10:
-                        break
+                    try:
+                        categories_tags = response['products'][product_index]['categories_hierarchy'][1]
+                        cleaned_cat = categories_tags.split(':')
+                        get_cat = str(cleaned_cat[1])
+                    except KeyError:
+                        get_cat = ''
 
                     Products.objects.create(name=get_name,
                                             nutriscore=get_nutriscore,
+                                            category=get_cat,
                                             picture=get_img,
                                             url=get_url)
 
-        product_list = Products.objects.filter(name__icontains=query).order_by('nutriscore')
-        paginator = Paginator(product_list, 3)
-        page = request.GET.get('page')
+                    if product_index == 15:
+                        break
+
+        filteringbycategory = Products.objects.filter(name__icontains=query)
+        cat = []
+        for i in filteringbycategory:
+            cat.append(i.category)
 
         try:
-            product = paginator.page(page)
-        except PageNotAnInteger:
-            product = paginator.page(1)
+            product_list = Products.objects.filter(category__icontains=random.choice(cat)) \
+                .order_by('nutriscore', 'name')
+        except:
+            product_list = Products.objects.filter(name__icontains=query) \
+                .order_by('nutriscore', 'name')
 
-        except EmptyPage:
-            product = paginator.page(paginator.num_pages)
-
+        product = customizePagination(request, product_list, 6)
         context = {"product": product,
                    "urlp": query}
 
