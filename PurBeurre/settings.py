53711edaba333ee8698dcfa1081fdd02edf7350a
CoLoDot <colodotgo@gmail.com>
2019-04-15 14:58:48 +0200

    correct syntax

--- PurBeurre/settings.py
+++ PurBeurre/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0hm-)!+0n&!f8^=$+l1twf(+@0x9kcpd-o^&fm-+a+kt*-c8_*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if os.environ.get('ENV') == 'PRODUCTION':
