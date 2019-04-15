5ea5c4dee9a3b27dc4c476ad298f912f8194fd2c
CoLoDot <colodotgo@gmail.com>
2019-04-15 10:03:39 +0200

    udpate migrations

--- substitut/migrations/0026_auto_20190411_1834.py
+++ substitut/migrations/0026_auto_20190411_1834.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='products',
             name='category',
-            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=900), blank=True, default=list, size=None),
+            field=django.contrib.postgres.fields.ArrayField(base_field=models.CharField(max_length=900), blank=True, default=list, size=None, null=True),
         ),
     ]
